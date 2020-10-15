6702393cef933eacfac7b5de6d1090b9279503df
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-10-15 23:47:05 +0200

    詳細画面のビューの作成

--- app/views.py
+++ app/views.py
@@ -1,11 +1,19 @@
-from django.view.generic import View
+from django.views.generic import View
+
 from django.shortcuts import render
 from .models import Post
 
 # Create your views here.
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        post_data = Post.object.order_by("-id")
+        post_data = Post.objects.order_by("-id")
         return render(request, 'app/index.html', {
             'post_data' : post_data,
+        })
+
+class PostDetailView(View):
+    def get(self, request, *args, **kwargs):
+        post_data = Post.objects.get(id=self.kwargs['pk'])
+        return render(request, 'app/post_detail.html', {
+            'post_data': post_data
         })
