2c096bfc2802458eb41ae93eca5e6ad270052566
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-10-15 23:16:08 +0200

    ビューフォルダ作成

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
+from django.view.generic import View
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        post_data = Post.object.order_by("-id")
+        return render(request, 'app/index.html', {
+            'post_data' : post_data,
+        })
